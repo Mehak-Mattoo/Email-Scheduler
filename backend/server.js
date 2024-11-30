8cc1fc5712740a8daaad635ca8a67a5d02920096
Mehak <mehakmattoo7@gmail.com>
2024-11-30 15:41:12 +0530

    minor

--- backend/server.js
+++ backend/server.js
@@ -1,72 +1,4 @@
-// const express = require("express");
-// const nodemailer = require("nodemailer");
-// const bodyParser = require('body-parser');
-// const { Agenda } = require('agenda');
 
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(express.json());
-// require("dotenv").config();
-// const cors = require('cors');
-// app.use(cors());
-
-// const agenda = new Agenda({ db: { address: process.env.MONGO_DB, collection: 'jobs' } });
-
-// const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   agenda.define('send email', async (job, done) => {
-//     const { to, subject, body } = job.attrs.data;
-
-//     const mailOptions = {
-//       from: process.env.EMAIL_USER,
-//       to,
-//       subject,
-//       text: body,
-//     };
-
-//     try {
-//       await transporter.sendMail(mailOptions);
-//       console.log('Email sent to:', to);
-//       done();
-//     } catch (error) {
-//       console.error('Error sending email:', error);
-//       done(error);
-//     }
-//   });
-
-//     app.post('/schedule-email', async (req, res) => {
-//         const { to, subject, body , delay} = req.body;
-
-//         // Validate request
-//         if (!to || !subject || !body|| !delay) {
-//           return res.status(400).json({ success: false, message: 'Missing request data' });
-//         }
-//  // Convert delay in hours to milliseconds
-//  const delayInMilliseconds = delay * 60 * 60 * 1000;
-
-//         try {
-//             // Schedule the email to be sent after delay
-//             await agenda.schedule(`in ${delay} hours`, 'send email', { to, subject, body });
-
-//             res.status(200).json({ success: true, message: `Email scheduled successfully to be sent in ${delay} hour(s)` });
-//           } catch (error) {
-//             console.error('Error scheduling email:', error);
-//             res.status(500).json({ success: false, message: 'Failed to schedule email', error: error.message });
-//           }
-//         });
-
-//         agenda.start();
-
-//   const PORT = process.env.PORT||8080;
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-//   });
 
 const express = require("express");
 const nodemailer = require("nodemailer");
@@ -156,7 +88,9 @@ app.post("/schedule-email", async (req, res) => {
 agenda.start();
 
 const PORT = process.env.PORT || 8080;
-
+app.get("/", (req, res) => {
+  res.send("Server is running. Welcome!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
